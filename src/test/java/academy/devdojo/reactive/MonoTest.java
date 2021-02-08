e954e0ac7e402b40177edb736880da1c0de4ad67
elivelton gustavo da silva <elivelton.gustavo34@gmail.com>
2021-02-08 10:38:33 -0300

    Subscribing to Mono

--- src/test/java/academy/devdojo/reactive/MonoTest.java
+++ src/test/java/academy/devdojo/reactive/MonoTest.java
@@ -2,12 +2,37 @@ package academy.devdojo.reactive;
 
 import lombok.extern.slf4j.Slf4j;
 import org.junit.jupiter.api.Test;
+import reactor.core.publisher.Mono;
+import reactor.test.StepVerifier;
 
 @Slf4j
+/**
+ * Reactive streams
+ * 1. Asynchronous
+ * 2. Non-blocking
+ * 3. Backpressure
+ * Publisher <- (subscribe) Subscriber
+ * Subscription is created
+ * Publisher (onSubscribe with the subscription) -> Subscriber
+ * Subscription <- (request N) Subscriber
+ * Publisher -> (onNext) Subscriber
+ * until:
+ * 1. Publisher sends all the objects requested.
+ * 2. Publisher sends all the objects it has. (onComplete) subscriber and subscription will be canceled
+ * 3. There is an error. (onError) -> subscriber and subscription will be canceled
+ */
 public class MonoTest {
 
     @Test
-    public void test() {
-        log.info("Everything working as intended");
+    public void monoSubscriber() {
+        String name = "Elivelton Gustavo";
+        Mono<String> mono = Mono.just(name)
+                .log();
+
+        mono.subscribe();
+        log.info("-----------------------------------------");
+        StepVerifier.create(mono)
+                .expectNext(name)
+                .verifyComplete();
     }
 }
